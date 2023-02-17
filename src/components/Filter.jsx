7ef65aa751ef14413e0473170cd085ae4a1240dd
Olivia Radick <olivia.radick@gmail.com>
2023-02-17 11:49:48 -0500

    home page search form

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -14,14 +14,18 @@ function Filter({ priceFilter, setPriceFilter, categoryFilter, setCategoryFilter
     }
 
     return (
-    <div>
+    <div className="search-bars">
+        <h1>Search Bars</h1>
+        <label>Price:</label>
         <select name="price-filter" value={priceFilter} onChange={handlePriceFilter}>
             <option value="all">All</option>
             <option value="$">$</option>
             <option value="$$">$$</option>
             <option value="$$$">$$$</option>
             <option value="$$$$">$$$$</option>
-        </select> <br/>
+        </select>
+        <br/>
+        <label>Category:</label>
         <select name="category-filter" value={categoryFilter} onChange={handleCategoryFilter}>
             <option value="all">All</option>
             <option value="tavern">Tavern</option>
@@ -30,7 +34,9 @@ function Filter({ priceFilter, setPriceFilter, categoryFilter, setCategoryFilter
             <option value="wine bar">Wine Bar</option>
             <option value="dive bar">Dive Bar</option>
             <option value="hotel bar">Hotel Bar</option>
-        </select> <br/>
+        </select>
+        <br/>
+        <label>Name:</label>
         <input name="name-filter" placeholder="Search bar name" onChange={handleNameFilter}/>
 
     </div>
