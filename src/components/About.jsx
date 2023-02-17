35025ff265e1ddb87a5d685a7255bee51c34db26
Avinash Ramsumair <avi@pure235games.com>
2023-02-17 10:54:00 -0500

    starting saving events

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -35,7 +35,7 @@ export default function About({setLoggedInUser}){
             </div>
             <div className="about-container">
             <img className="about-logo" src={logo1} />
-            <p>A phase 3 and 4 project made by Avi, Daniel, Olivia, and Tomer.</p> 
+            <p>A project made by Avi, Daniel, Olivia, and Tomer.</p> 
             <p id="dedication">In Memoriam of: James Prawdzik</p>
             </div>
         </div>        
--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -13,7 +13,6 @@ export default function Account ({loggedInUser, setLoggedInUser}){
     const [toggleLookFriendsRequests, setToggleLookFriendsRequests] = useState(false);
     const [toggleNewFriendSearch, setToogleNewFriendSearch] = useState(false);
 
-    console.log(loggedInUser);
 
     //fetch all the reviews
     const fetchReviews = async () => {
